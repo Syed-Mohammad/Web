42addb664af8402db26bbd18f9915706eb47a83b
Syed-Mohammad <52983622+Syed-Mohammad@users.noreply.github.com>
2021-02-13 17:01:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const mySelf = {
+  name: 'Syed Mohammad Hasan',
+  age: 23,
+  profession: 'Software Developer',
+  skills: 'HTML, CSS, JavaScript, TypeScript, Python',
+};
+
+console.log(mySelf.profession);
